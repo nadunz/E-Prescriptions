e588f553e05d8bd8fe5131b11ebf8737e97992c6
nadunz <nadunp94@gmail.com>
2018-12-29 11:33:14 +0530

    doctor app template

--- DoctorApp/src/app/app-routing.module.ts
+++ DoctorApp/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+      path: '',
+      loadChildren: './pages/pages.module#PagesModule'
+  },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), NgbModule.forRoot()],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- DoctorApp/src/app/app.module.ts
+++ DoctorApp/src/app/app.module.ts
@@ -1,18 +1,33 @@
+import * as $ from 'jquery';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { Routes, RouterModule } from '@angular/router';
+import { SpinnerComponent } from './shared/spinner.component';
+
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, SpinnerComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LocationStrategy,
+      useClass: PathLocationStrategy
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
